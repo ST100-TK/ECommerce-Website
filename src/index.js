18986a71f6f0936964b4d8bfb604b8e095fbe2ef
Thebe Khabo <thebe.khabo1@gmail.com>
2026-02-26 21:53:50 +0200

    Restrictions on Login and Signup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const collection = require('./config');
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
+
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.set('view engine', 'ejs');
@@ -16,6 +20,45 @@ app.get('/', (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.render("signup");
+});
+
+app.post('/signup', async (req, res) => {
+    const data = {
+        username: req.body.username,
+        password: req.body.password,
+    }
+
+    const existingUser = await collection.findOne({ username: data.username });
+
+    if(existingUser) {
+        res.send('Username already exists. Please choose a different username.');
+    }else{
+        const saltRounds = 10;
+        const hashedPassword = await Bcrypt.hash(data.password, saltRounds);
+        data.password = hashedPassword;
+        const userdata = await collection.insertMany(data);
+        console.log(userdata)
+    }
+
+});
+
+app.post('/login', async (req, res) => {
+    try{
+        const check = await collection.findOne({ username: req.body.username });
+        if(!check){
+            res.send('Invalid username');
+       }
+
+        const passwordMatch = await Bcrypt.compare(req.body.password, check.password);
+        if(passwordMatch){
+            res.redirect('/home');
+        }else{
+            res.send('Invalid password');
+        }
+    }catch{
+        res.send("Wrong Details");
+    }   
+
 });
 
 const port = 1500;
